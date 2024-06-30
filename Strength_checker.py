6030f5ffc0662c059f0f6a83999943ba212f62cd
Garv Pankaj <161364091+silent-garv@users.noreply.github.com>
2024-06-30 23:25:43 +0530

    Update Strength_checker.py

--- Strength_checker.py
+++ Strength_checker.py
@@ -1,15 +1,16 @@
-import re
+#Password strength checker 
+import re   # importing regular expressions 
 
 password = input("Enter your password :")
-if len(password) < 8:
+if len(password) < 8:   # condition to check length of password 
     print("Password must be at least 8 character long.")
-elif not re.search("[A-Z]",password):
+elif not re.search("[A-Z]",password):  # condition to add Uppercase in password 
     print("Password must contain at least one uppercase letter")
-elif not re.search("[a-z]",password):
+elif not re.search("[a-z]",password):   # condition to add Lowercase in password 
     print("Password must contain at least one lowercase letter")
-elif not re.search("[0-9]",password):
+elif not re.search("[0-9]",password):  # condition to add number in password 
     print("Password must contain one number")
-elif not re.search("[@,!,#,$,%,^,&,*]",password):
+elif not re.search("[@,!,#,$,%,^,&,*]",password):   # conditionto add special character in password 
     print("Password must contain at least one special character")    
 else:
     print("password is strong")        
